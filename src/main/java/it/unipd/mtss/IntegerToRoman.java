11ceeec5df7cb0856d0e72c0e7364b27aeca3a3d
tommasozorzetto <tommaso.zorzetto@studenti.unipd.it>
2026-05-05 16:26:03 +0200

    Implementato convertitore IntegerToRoman (Issue #1)

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,28 @@
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+
+    public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException("Il numero deve essere compreso tra 1 e 1000");
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+
+        // Scorriamo tutti i valori disponibili dal più grande al più piccolo
+        for (int i = 0; i < values.length; i++) {
+            // Finché il numero da convertire è maggiore o uguale al valore corrente
+            while (number >= values[i]) {
+                // aggiungi il simbolo romano al risultato
+                result.append(romanSymbols[i]);
+                // sottrai il valore dal numero di partenza
+                number -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+}
